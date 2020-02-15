5ae1800471ebc1d9623abf4c713154174342ba16
Caden Wang <cawa7995@gmail.com>
2020-02-14 19:24:52 -0800

    renders list of projects on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
+const request = require('request');
+const axios = require('axios');
+const apiKey = require('../config.js').hackaday.apiKey;
 const indexRoute = {};
 
+
+
 indexRoute.get = function (req, res, next) {
-    return res.render('index');
+    axios.get(`http://api.hackaday.io/v1/projects?api_key=${apiKey}&sortby=newest`).then(data => {
+        // console.log(data.data.projects);
+        return res.render('index', {data: data.data});
+    }).catch(err => {
+        console.log('failed to get data', data)
+    })
+    
 }
 
 module.exports = indexRoute;
