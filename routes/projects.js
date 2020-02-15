a1483bafec09a37018ca18dab0d7406b7f030c25
Caden Wang <cawa7995@gmail.com>
2020-02-14 19:29:59 -0800

    dynamically renders specific projects for /projects/:id

--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,17 @@
 const projectRoute = {};
+const axios = require('axios');
+const apiKey = require('../config.js').hackaday.apiKey;
 
 projectRoute.get = function (req, res, next) {
-    const projectId = req.params.id;
-    return res.render('project', { projectId: projectId });
+    let projectId = req.params.id;
+    axios.get(`http://api.hackaday.io/v1/projects/${projectId}?api_key=${apiKey}`).then(data => {
+        // console.log(data.data);
+
+        return res.render('project', { projectId: projectId, project: data.data });
+    }).catch(err => {
+        console.log('failed to get data PROJECTS.JS', data)
+    })
+    
 }
 
 module.exports = projectRoute;
